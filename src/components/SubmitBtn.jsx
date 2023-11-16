2397166f1529ffd75b5c6be2872a2672f10817ae
Viktor Filippov <jackal3166@gmail.com>
2023-11-16 14:51:53 +0200

    Create Register

--- src/components/SubmitBtn.jsx
+++ src/components/SubmitBtn.jsx
@@ -1,8 +1,19 @@
+import { useNavigation } from 'react-router-dom';
 import { PropTypes } from 'prop-types';
+
 const SubmitBtn = ({ text }) => {
+	const navigation = useNavigation();
+	const isSubmitting = navigation.state === 'submitting';
+
 	return (
-		<button type='submit' className='btn-primary'>
-			{text}
+		<button type='submit' className='uppercase btn-primary btn btn-block'>
+			{isSubmitting ? (
+				<>
+					<span className='loading loading-bars'></span>sending...
+				</>
+			) : (
+				text || 'submit'
+			)}
 		</button>
 	);
 };
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,43 @@
+import { Form, Link } from 'react-router-dom';
+import FormInput from '../components/FormInput';
+import SubmitBtn from '../components/SubmitBtn';
+
 const Login = () => {
-	return <div>Login</div>;
+	return (
+		<section className='grid h-screen place-items-center'>
+			<Form
+				method='POST'
+				className='p-8 shadow shadow-primary card w-96 bg-base-100 gap-y-4'>
+				<h4 className='text-3xl font-bold text-center'>Login</h4>
+				<FormInput
+					type='email'
+					label='email'
+					name='identifier'
+					defaultValue='test@test.com'
+				/>
+				<FormInput
+					type='password'
+					label='password'
+					name='password'
+					defaultValue='secret'
+				/>
+				<div className='mt-4'>
+					<SubmitBtn text='login' />
+				</div>
+				<button className='uppercase duration-300 btn btn-block btn-secondary'>
+					guest user
+				</button>
+				<p className='text-center'>
+					Not a member yet?{' '}
+					<Link
+						to='/register'
+						className='ml-2 capitalize link link-hover link-primary'>
+						register
+					</Link>
+				</p>
+			</Form>
+		</section>
+	);
 };
 
 export default Login;
