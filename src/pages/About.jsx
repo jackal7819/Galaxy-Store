7e6940f0244aa14ecc6c16e59d2f4937eca8c722
Viktor Filippov <jackal3166@gmail.com>
2023-11-17 15:35:31 +0200

    Create About

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,37 @@
 const About = () => {
-	return <div>About</div>;
+	return (
+		<>
+			<div className='flex flex-wrap items-center justify-center gap-2 sm:gap-x-6'>
+				<h1 className='text-4xl font-bold leading-none tracking-tight sm:text-6xl'>
+					Best Comfort for
+				</h1>
+				<div className='shadow stats bg-primary'>
+					<div className='stat'>
+						<div className='text-4xl font-bold tracking-widest stat-title text-primary-content'>
+							home
+						</div>
+					</div>
+				</div>
+			</div>
+			<p className='max-w-2xl mx-auto mt-6 text-lg leading-8'>
+				Welcome to the world of Galaxy - where your dreams of stylish
+				and functional interior design come to life. With our team&#39;s
+				extensive experience, we strive to provide you with the finest
+				solutions for your home. We don&#39;t just sell furniture; we
+				create an atmosphere where every detail matters. Our range
+				includes exquisite sofas, stylish tables and chairs, elegant
+				cabinets, and much more. We take pride in offering products that
+				combine exceptional quality, refined design, and affordable
+				prices. At Galaxy, we aim to be your reliable partner in
+				creating a home where you delight in spending your time. Let us
+				bring your vision of modern design to life and make your home a
+				place where you truly feel comfortable. Join our community of
+				enthusiasts for beautiful furniture and design. Galaxy is not
+				just a place to shop; it&#39;s where your dreams of the perfect
+				home come to fruition.
+			</p>
+		</>
+	);
 };
 
 export default About;
