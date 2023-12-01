67d0cec49f16a3889752e050982b7dcd41a59cd3
Viktor Filippov <jackal3166@gmail.com>
2023-12-01 19:40:48 +0200

    Create CartTotals

--- src/components/CartTotals.jsx
+++ src/components/CartTotals.jsx
@@ -1,5 +1,36 @@
+import formatPrice from '../services/formatPrice';
+import { useSelector } from 'react-redux';
+
 const CartTotals = () => {
-	return <div>CartTotals</div>;
+	const { cartTotal, shipping, tax, orderTotal } = useSelector(
+		(state) => state.cart
+	);
+	return (
+		<div className='card bg-base-200'>
+			<div className='card-body'>
+				<p className='flex justify-between pb-2 text-xs border-b border-base-300'>
+					<span>Subtotal</span>
+					<span className='font-medium'>
+						{formatPrice(cartTotal)}
+					</span>
+				</p>
+				<p className='flex justify-between pb-2 text-xs border-b border-base-300'>
+					<span>Shipping</span>
+					<span className='font-medium'>{formatPrice(shipping)}</span>
+				</p>
+				<p className='flex justify-between pb-2 text-xs border-b border-base-300'>
+					<span>Tax</span>
+					<span className='font-medium'>{formatPrice(tax)}</span>
+				</p>
+				<p className='flex justify-between pb-2 mt-4 text-sm'>
+					<span>Order Total</span>
+					<span className='font-medium'>
+						{formatPrice(orderTotal)}
+					</span>
+				</p>
+			</div>
+		</div>
+	);
 };
 
 export default CartTotals;
