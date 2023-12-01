9306f913586789f2b3bd906d2ade10a426b406a6
Viktor Filippov <jackal3166@gmail.com>
2023-12-01 19:21:36 +0200

    Create Cart

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,5 +1,37 @@
+import CartItemsList from '../components/CartItemsList';
+import CartTotals from '../components/CartTotals';
+import { Link } from 'react-router-dom';
+import SectionTitle from '../components/SectionTitle';
+import { useSelector } from 'react-redux';
+
 const Cart = () => {
-	return <div>Cart</div>;
+	const user = null;
+	const numItemsInCart = useSelector((state) => state.cart.numItemsInCart);
+
+	if (numItemsInCart === 0) return <SectionTitle text='Your cart is empty' />;
+
+	return (
+		<>
+			<SectionTitle text='Shopping Cart' />
+			<div className='grid gap-8 mt-8 lg:grid-cols-12'>
+				<div className='lg:col-span-8'>
+					<CartItemsList />
+				</div>
+				<div className='lg:col-span-4'>
+					<CartTotals />
+					{user ? (
+						<Link
+							to='/checkout'
+							className='mt-8 btn btn-primary btn-block'>
+							proceed to checkout
+						</Link>
+					) : (
+						<Link className='mt-8 uppercase btn btn-primary btn-block'>please login</Link>
+					)}
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default Cart;
